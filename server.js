89d87fd8fbc83be3d50d0da9b63a28029dbd53cf
Vinayak.ojha47billion <vinayak.ojha@47billion.com>
2024-07-31 14:58:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World by Vinayakojha jenkins pipeline');
+  res.send('Hello World by Vinayakojha jenkins');
 });
 
 app.listen(PORT, HOST);
